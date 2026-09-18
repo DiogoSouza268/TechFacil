516e0706fa0f0866a4c97f37ed590b45249bdb21
DiogoSouza268 <dooss2085@gmail.com>
2026-09-18 10:43:28 -0300

    feat: cria as rotas para as paginas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Perfil } from './pages/perfil/perfil';
+import { Login } from './pages/login/login';
+import { Dashboard } from './pages/dashboard/dashboard';
+import { Cadastrar } from './pages/cadastrar/cadastrar';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: " ", redirectTo: "dashboard", pathMatch: "full"},
+    {path: "cadastrar", component: Cadastrar},
+    {path: "dashboard", component: Dashboard},
+    {path: "login", component: Login},
+    {path: "perfil", component: Perfil}
+];
